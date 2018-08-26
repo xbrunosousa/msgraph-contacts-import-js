6e8abc98d22bf23a12ff0bd64d01e999de494198
Bruno Vinícius <mail@bruno.vin>
2018-08-26 00:51:51 -0300

    Poluído, mas funcionando...(pra mim)

--- src/components/mixins/importContacts.vue
+++ src/components/mixins/importContacts.vue
@@ -1,5 +1,6 @@
 <script>
-import hello from 'hellojs';
+// import hello from 'hellojs-xbrunosousa';
+// import * as hello from "./../../assets/js/hello.all.js";
 export default {
   name: 'importContacts',
   // https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=837fe74f-e66b-441f-8728-088de9fc6670&redirect_uri=https://192.168.0.12:8080&response_type=token&scope=Contacts.Read
@@ -15,43 +16,13 @@ export default {
       token: undefined,
       tokenGetedStorage: undefined,
       resposta: [],
-      local: undefined
+      local: undefined,
+      oi: undefined
     };
   },
-  mounted() {
-    let item = localStorage.getItem('hello');
-    this.local = JSON.parse(item);
-
-    // console.log('​mounted -> ', 'wtffffff');
-    // let urlWindow = window.location.href;
-    // if (urlWindow.search('access_token') !== -1) {
-    //   let url = new URL(urlWindow);
-    //   this.token = url.hash
-    //     .replace('&token_type=bearer&expires_in=3600&scope=Contacts.Read', '')
-    //     .replace('#access_token=', '');
-    //   localStorage.setItem('tokenMicrosofttt', this.token);
-    // }
-    // if (localStorage.getItem('tokenMicrosofttt') !== undefined) {
-    //   this.tokenGetedStorage = localStorage.getItem('tokenMicrosoft');
-    // }
-  },
+  mounted() {},
   methods: {
     outroTeste() {
-      // let popup = window.open(
-      //   'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=837fe74f-e66b-441f-8728-088de9fc6670&redirect_uri=http://localhost:8080&response_type=token&scope=Contacts.Read',
-      //   '',
-      //   'width=350, height=255, top=100, left=110, scrollbars=no '
-      // );
-      // let teste = setInterval(() => {
-      //   if (popup.closed) {
-      //     console.log('Fechada');
-      //     console.log(popup);
-      //     clearInterval(teste);
-      //   } else {
-      //     console.log(popup);
-      //     console.log('aberta');
-      //   }
-      // }, 2000);
       hello.init(
         {
           windows: this.clientId
@@ -63,39 +34,35 @@ export default {
       );
       hello('windows')
         .login({ scope: 'Contacts.Read' })
-        .then(
-          function(auth) {
-            // Get the friends
-            // using path, me/friends or me/contacts
-            hello('windows')
-              .api('me/contacts')
-              .then(function responseHandler(r) {
-                for (var i = 0; i < r.data.length; i++) {
-                  var o = r.data[i];
-                  console.log(o)
-                }
-
-              });
-          },
-          function() {
-            if (!auth || auth.error) {
-              console.log('Signin aborted');
-              return;
-            }
-          }
-        );
+        .then((res) => {
+          this.$snotify.success('Wiiii! 👨‍💻', '');
+          this.local = JSON.parse(item);
+          console.log(this.local);
+          console.log(res);
+          let item = localStorage;
+        })
+        .catch(() => {
+          setTimeout(() => {
+            // console.clear();
+          }, 100);
+        })
+        .finally(() => {
+          console.log('​outroTeste -> ', JSON.parse(localStorage.getItem('windows')));
+          this.token = JSON.parse(localStorage.getItem('windows')).access_token;
+        });
     },
     runMicrosoft() {
-      var client = MicrosoftGraph.Client.init({
-        authProvider: done => {
-          done(null, this.tokenNew);
+      fetch('https://graph.microsoft.com/beta/me/contacts?$select=emailaddresses', {
+        headers: {
+          Authorization: `Bearer ${this.token}`
         }
-      });
-
-      client.api('/me/contacts?$select=emailaddresses').get((err, res) => {
-        this.resposta = res;
-        console.log(res);
-      });
+      })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+      }).catch(() => {
+        this.$snotify('', 'Deu ruim!');
+      })
     }
   }
 };
